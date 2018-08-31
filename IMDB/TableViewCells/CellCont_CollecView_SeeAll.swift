566eabd36f346722d9639de579becafc23e4c531
Mohamed Mohsen <Mohamed.Ebrahim.Mohsen@gmail.comgit config --global user.name MohamedEbrahimMohsen>
2018-08-31 16:38:42 +0200

    Starting Developing IMDB App

---
+++ IMDB/TableViewCells/CellCont_CollecView_SeeAll.swift
@@ -0,0 +1,24 @@
+//
+//  CellCont_CollecView_SeeAll.swift
+//  IMDB
+//
+//  Created by Mohamed Mohsen on 8/31/18.
+//  Copyright © 2018 Mohamed Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class CellCont_CollecView_SeeAll: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
